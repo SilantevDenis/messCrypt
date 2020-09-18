046f7b66c0f122a1e224d57ed7ef9eb35c6ed584
Denis Silantev <d.silantjev2012@gmail.com>
2020-09-19 01:28:02 +0300

    First code

---
+++ messCrypt.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         messCrypt
+// @namespace
+// @version      1.0
+// @description  encryption of social media messages
+// @author       silantevdenis
+// @match        *://*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.5.1.slim.js
+// @require      https://raw.githubusercontent.com/SilantevDenis/Remove-ads-on-vc-dtf-tj/master/scripts.js
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  scripts();
+})();
